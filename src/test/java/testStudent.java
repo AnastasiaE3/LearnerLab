5f9c2102115cfe80d5308b1bee0da506a736fa72
AnastasiaE3 <epiart2000@gmail.com>
2023-08-13 14:12:47 -0400

    finished last test for student

--- src/test/java/testStudent.java
+++ src/test/java/testStudent.java
@@ -23,6 +23,12 @@ public class testStudent {
 
     @Test
     public void testLearner() {
-
+        student student = new student(1, "Nastya");
+        // The expected value is 5.0, the actual value is student.getTotalStudyTime(),
+        // and 0.0001 is the tolerance (or delta) allowed in the comparison.
+        // This means that the test will consider the values equal if the difference
+        // between the expected and actual values is within 0.0001.
+        student.learn(5.0);
+        assertEquals(5.0, student.getTotalStudyTime(), 0.0001);
     }
 }
