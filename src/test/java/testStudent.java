bb53de66b11960b1f4c33e07e5475fffd60c1bbd
AnastasiaE3 <epiart2000@gmail.com>
2023-08-13 13:53:27 -0400

    passed one test in student class

--- src/test/java/testStudent.java
+++ src/test/java/testStudent.java
@@ -1,14 +1,16 @@
-package io.zipcoder.interfaces;
+
 
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 
 public class testStudent {
     @Test
     public void testImplementation() {
- // test comment
+        student student = new student(1, "Nastya");
+        assertTrue(student instanceof learner);
     }
 
     @Test
